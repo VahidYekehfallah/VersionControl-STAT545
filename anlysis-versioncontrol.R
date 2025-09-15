58f243ba9f0b95020546895bc0957b5cc6eed413
VahidYekehfallah <vahidyekkefallah@gmail.com>
2025-09-15 15:34:18 -0700

    Initial commit

---
+++ anlysis-versioncontrol.R
@@ -0,0 +1,8 @@
+# view the first rows of mtcars dataset
+head(mtcars)
+
+# calculate the average mpg among all cars, save it as variable
+mean_mpg <- mean(mtcars$mpg)
+
+#print the mean mpg
+mean_mpg
